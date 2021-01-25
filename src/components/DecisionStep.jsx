73d14276c47ae973b5a4c74e39721f1f86b88aa4
Susan Song <ssong@baltimorecountymd.gov>
2021-01-25 15:28:41 -0500

    Fix button and add step indicator

--- src/components/DecisionStep.jsx
+++ src/components/DecisionStep.jsx
@@ -1,30 +1,43 @@
 import React from "react";
 import ReactHtmlParser from "react-html-parser";
-import { Button } from "@baltimorecounty/dotgov-components";
+import { Button, Section } from "@baltimorecounty/dotgov-components";
 
 const DecisionTree = (props) => {
-  var choiceElements = [];
-  
-  const handlesOnClick = () => {
-    props.nextStep();
-  };
 
+  var choiceElements = [];
   props.choices.forEach((choice, i) => {
     if (props.type === "Button") {
+        const handlesOnClickGo = () => {
+            props.goToStep(choice.GoTo);
+          };
+
       choiceElements.push(
         <Button
           key={i}
           text={choice.ChoiceText}
-          //onClick={props.goToStep(choice.GoTo)}
-          onClick={handlesOnClick}
+          onClick={handlesOnClickGo}
         />
       );
     }
   });
 
+  var stepIndicatorElements = [];
+  for (var i = 1; i <= props.totalSteps; i++) {
+       var indicatorColor;
+      if (i < props.currentStep) {indicatorColor = "#F7F7F7";}
+      if (i === props.currentStep) {indicatorColor = "#FFD986";}
+      if (i > props.currentStep) {indicatorColor = "#8091C0";}
+      stepIndicatorElements.push(
+        <div key={i} style={{display: "inline", color: indicatorColor}}>
+            <i className="fas fa-circle" aria-hidden="true"></i>
+            <span>{i}</span>
+        </div>
+      )
+  }
+
   return (
-    <div>
-      {/* <section className="dg_section dark"> */}
+
+      <Section className="dg_section dark">
       <div className="container">
         <div className="row">
           <div className="col">
@@ -36,11 +49,13 @@ const DecisionTree = (props) => {
             </div>
             <div className="text">{ReactHtmlParser(props.text)}</div>
             {choiceElements}
+            <br />
+            {stepIndicatorElements}
           </div>
         </div>
       </div>
-      {/* </section> */}
-    </div>
+      </Section>
+
   );
 };
 
